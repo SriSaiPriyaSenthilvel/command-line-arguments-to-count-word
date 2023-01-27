ea778c03d0869f340978fd399d570589557c2a10
SriSaiPriyaSenthilvel <srisai4405@gmail.com>
2023-01-27 11:53:48 +0530

    added

---
+++ command.py
@@ -0,0 +1,12 @@
+import sys
+d={}
+f=open(sys.argv[1],"r")
+for line in f:
+    l=line.split(" ")
+    for word in l:
+        if word not in d:
+            d[word]=1
+        else:
+            d[word]+=1
+print(d)
+f.close()
